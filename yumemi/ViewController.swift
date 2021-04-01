baa7be2ad07b6fa28f77efb1f41f4b0b3b34d92f
riku3 <takarakuten62@gmail.com>
2021-04-02 07:26:26 +0900

    Initial Commit

---
+++ yumemi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi
+//
+//  Created by riku on 2021/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
