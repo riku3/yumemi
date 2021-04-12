b3cdead224cc6390ea358d3c3496c7927e8bc4d9
riku3 <takarakuten62@gmail.com>
2021-04-12 13:08:36 +0900

    天気の画像をランダムに表示

--- yumemi/ViewController.swift
+++ yumemi/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
+    
+//    enum Weather: String {
+//        case sunny = "sunny"
+//        case cloudy = "cloudy"
+//        case rainy = "rainy"
+//    }
 
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    var yumemiAPI = YumemiWeather.self
+    var loadingAPI = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // ランダムな天気画像を表示する
+        setWeatherImage()
     }
-
-
+    
+    @IBAction func tapReload(_ sender: UIButton) {
+        // ランダムな天気画像を表示する
+        setWeatherImage()
+    }
+    
+    private func setWeatherImage() {
+        guard !loadingAPI else {
+            return
+        }
+        
+        loadingAPI = true
+        let weather = yumemiAPI.fetchWeather()
+        
+        switch weather {
+        case "sunny":
+            weatherImageView.image = UIImage(named: "sunny");
+        case "cloudy":
+            weatherImageView.image = UIImage(named: "cloudy");
+        case "rainy":
+            weatherImageView.image = UIImage(named: "rainy");
+        default:
+            break
+        }
+        
+        loadingAPI = false
+    }
 }
 
