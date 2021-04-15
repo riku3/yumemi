61764dc009c0f8b310e9efde8a48f5ed12d2e0cf
riku3 <takarakuten62@gmail.com>
2021-04-15 15:53:34 +0900

    処理分利用に新規クラスを作成

---
+++ yumemi/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  yumemi
+//
+//  Created by riku on 2021/04/15.
+//
+
+import Foundation
---
+++ yumemi/WeatherModelImpl.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModelImpl.swift
+//  yumemi
+//
+//  Created by riku on 2021/04/15.
+//
+
+import Foundation
